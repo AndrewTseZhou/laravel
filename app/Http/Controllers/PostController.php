7db25fdee1f742b80b081ae0b5a1eff870549f24
谢康 <xk@meitu.com>
2019-01-08 19:39:36 +0800

    文章详情页实现

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -18,8 +18,8 @@ class PostController extends Controller {
     }
 
     //文章详情页
-    public function show() {
-        return view("post/show", ['title' => 'this is title', 'isShow' => false]);
+    public function show(Post $post) {
+        return view("post/show", compact('post'));
     }
 
     //创建文章
