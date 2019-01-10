469d1aa181b1e40a2a8c8b63052b2dc91e0e51f0
谢康 <xk@meitu.com>
2019-01-10 21:17:56 +0800

    用户注册逻辑实现

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\User;
+
 class RegisterController extends Controller {
     //注册页面
     public function index() {
@@ -10,6 +12,20 @@ class RegisterController extends Controller {
 
     //注册行为
     public function register() {
+        //验证
+        $this->validate(request(), [
+            'name' => 'required|min:3|unique:users,name',
+            'email' => 'required|unique:users,email|email',
+            'password' => 'required|min:5|max:10|confirmed',
+        ]);
 
+        //逻辑
+        $name = request('name');
+        $email = request('email');
+        $password = bcrypt(request('password'));
+        $user = User::create(compact('name', 'email', 'password'));
+
+        //渲染
+        return redirect('/login');
     }
 }
