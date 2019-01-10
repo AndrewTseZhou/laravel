a010a97b938455a278a98c2f6109be83ada386da
谢康 <xk@meitu.com>
2019-01-10 21:01:55 +0800

    创建登录、注册、个人设置三个页面

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class UserController extends Controller {
+    //个人设置页面
+    public function setting() {
+        return view('user.setting');
+    }
+
+    //个人设置行为
+    public function settingStore() {
+
+    }
+}
