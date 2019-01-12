37ec6bcaba69211d0bb0af378d103f5b3f327008
谢康 <xk@meitu.com>
2019-01-12 14:10:00 +0800

    完善post表，创建评论表

--- database/migrations/2019_01_12_135844_create_comment_table.php
+++ database/migrations/2019_01_12_135844_create_comment_table.php
@@ -4,18 +4,18 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreatePostsTable extends Migration {
+class CreateCommentTable extends Migration {
     /**
      * Run the migrations.
      *
      * @return void
      */
     public function up() {
-        Schema::create('posts', function (Blueprint $table) {
+        Schema::create('comments', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('title')->default("");
-            $table->text("content");
             $table->integer('user_id')->default(0);
+            $table->integer('post_id')->default(0);
+            $table->text('content');
             $table->timestamps();
         });
     }
@@ -26,6 +26,6 @@ class CreatePostsTable extends Migration {
      * @return void
      */
     public function down() {
-        Schema::dropIfExists('posts');
+        Schema::dropIfExists('comments');
     }
 }
