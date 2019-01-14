885e2cfdc28f0cda0eaeafa65ad110ddcf9447c6
谢康 <xk@meitu.com>
2019-01-14 17:47:31 +0800

    实现多对多模型关联

--- app/Fan.php
+++ app/Fan.php
@@ -3,5 +3,14 @@
 namespace App;
 
 class Fan extends BaseModel {
-    //
+
+    //粉丝用户
+    public function fuser() {
+        return $this->hasOne(User::class, 'id', 'fan_id');
+    }
+
+    //被关注的用户
+    public function suser() {
+        return $this->hasOne(User::class, 'id', 'star_id');
+    }
 }
