98b87d14135143ae95a6605978992714702628fa
谢康 <xk@meitu.com>
2019-01-15 20:34:36 +0800

    专题详情页逻辑实现

--- app/Http/Controllers/TopicController.php
+++ app/Http/Controllers/TopicController.php
@@ -2,14 +2,27 @@
 
 namespace App\Http\Controllers;
 
+use App\Post;
 use App\Topic;
+use Illuminate\Support\Facades\Auth;
 
 class TopicController extends Controller {
 
-    public function show() {
-        return view('topic/show');
+    //专题详情页
+    public function show(Topic $topic) {
+        //带文章数的专题
+//        $topic = Topic::withCount('postTopics')->find($topic->id);
+
+        //专题的文章列表，按照创建时间倒序排序
+        $posts = $topic->posts()->orderBy('created_at', 'desc')->paginate(10);
+
+        //属于我的文章，但是未投稿
+        $myposts = Post::authorBy(Auth::id())->topicNotBy($topic->id)->get();
+
+        return view('topic/show',compact('topic','posts','myposts'));
     }
 
+    //投稿
     public function submit(Topic $topic) {
 
     }
