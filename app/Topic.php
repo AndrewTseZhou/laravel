35b49f87497e0ad4e42e44be8791bab13c42067d
谢康 <xk@meitu.com>
2019-01-15 18:32:57 +0800

    文章和专题模型关联

--- app/Topic.php
+++ app/Topic.php
@@ -3,5 +3,14 @@
 namespace App;
 
 class Topic extends BaseModel {
-    //
+
+    //属于这个专题的所有文章
+    public function posts() {
+        return $this->belongsToMany(Post::class, 'post_topics', 'topic_id', 'post_id');
+    }
+
+    //专题的文章数
+    public function postTopics() {
+        return $this->hasMany(PostTopic::class, 'topic_id', 'post_id');
+    }
 }
