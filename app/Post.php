638925f8ac7cf76d2e731c68cfda15f14b58475a
谢康 <xk@meitu.com>
2019-01-15 18:26:46 +0800

    scope实现某个用户未投稿的文章

--- app/Post.php
+++ app/Post.php
@@ -3,6 +3,7 @@
 namespace App;
 
 //表=>Posts
+use Illuminate\Database\Eloquent\Builder;
 use Laravel\Scout\Searchable;
 
 class Post extends BaseModel {
@@ -42,5 +43,21 @@ class Post extends BaseModel {
     //文章的所有赞
     public function zans() {
         return $this->hasMany('App\Zan');
+    }
+
+    //属于某个作者的文章
+    public function scopeAuthorBy(Builder $query, $user_id) {
+        return $query->where('user_id', $user_id);
+    }
+
+    public function postTopics() {
+        return $this->hasMany(PostTopic::class, 'post_id', 'id');
+    }
+
+    //不属于某个专题的文章
+    public function scopeTopicNotBy(Builder $query, $topic_id) {
+        return $query->doesntHave('postTopics', 'and', function ($q) use ($topic_id) {
+            $q->where('topic_id', $topic_id);
+        });
     }
 }
